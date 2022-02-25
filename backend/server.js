067c4cb5ded9690b7b7f7dd64a7a1a56b61b3575
Rhonda Crouse <rhondacrouse@developersinstitute.co.nz>
2022-02-25 18:50:07 +1300

    set up the routes

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use('/api/coeliacs', require('./routes/coeliacRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
