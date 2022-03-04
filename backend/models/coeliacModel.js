8e8b15b3219fcd644083dcfbfb1af3f03d7be30d
Rhonda Crouse <rhondacrouse@developersinstitute.co.nz>
2022-03-05 10:05:10 +1300

    changed the Schema

--- backend/models/coeliacModel.js
+++ backend/models/coeliacModel.js
@@ -7,29 +7,22 @@ const coeliacSchema = mongoose.Schema(
       required: true,
       ref: 'User',
     },
-
-    text: {
+  },
+  {
+    restaurantName: {
       type: String,
-      required: [true, 'Please fill in the text'],
+      required: [true, 'Please input some text'],
     },
+    address: { type: String, required: [true, 'Please input some text'] },
+    suburb: { type: String, required: [true, 'Please input some text'] },
+    country: { type: String, required: [true, 'Please input some text'] },
+    cuisine: { type: String },
+    priceRange: { type: Number },
+    review: { type: String, required: [true, 'Please input some text'] },
   },
   {
     timestamps: true,
   }
-  //   {
-  //  restaurantName: {
-  //       type: String,
-  //       required: [true, 'Please input some text'],
-  //     },
-  //     address: { type: String, required: [true, 'Please input some text'] },
-  //     suburb: { type: String, required: [true, 'Please input some text'] },
-  //     country: { type: String, required: [true, 'Please input some text'] },
-  //     cuisine: { type: String },
-  //     priceRange: { type: Number },
-  //     review: { type: String, required: [true, 'Please input some text'] },
-  //     date: { type: Date, required: [true, 'Please input some text'] },
-  //     username: { type: String, required: [true, 'Please input some text'] },
-  //   },
 );
 
 module.exports = mongoose.model('Coeliac', coeliacSchema);
